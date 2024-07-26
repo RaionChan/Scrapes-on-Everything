453f4d0c8c59912012d662eb6a7214c2bfca3e4c
RaionClaire <chacastastaria@gmail.com>
2024-07-26 14:01:36 +0700

    Modify scrapingMAL.py

--- scrapingMAL.py
+++ scrapingMAL.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import json
+import os
 
 print("Welcome to MyAnimeList Scraper!\nIt's hilarious because I need it to checked all of 'em effortlessly\nWell, here it is!")
 print("But beefore we start, let me ask, u wanna searh them manually or using ur plan to watch list? (manual/plan)")
@@ -75,11 +77,6 @@ if answer.lower() == 'manual':
         else:
             continue
 
-    print("================================")
-    print("Here is the list of anime you have searched: ")
-    df = pd.DataFrame(listOfAnime, columns=['Satisfaction', 'Title'])
-    print(df)
-    print("Thank you for using MyAnimeList Scraper! Have a nice day!")
 
 elif answer.lower() == 'plan':
     print("Alright, gimme ur username: ")
@@ -88,24 +85,61 @@ elif answer.lower() == 'plan':
     print("================================")
 
     response = requests.get(base_url)
-
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    titles = []
-    link = []
+    table = soup.find('table', class_='list-table')
+    data_items_json = table.get('data-items')
 
-    results = soup.find_all('a', class_='list-tables')
-    for result in results:
-        title = result.text
-        titles.append(title)
+    data_items_json = data_items_json.replace('&quot;', '"')
+    data_items = json.loads(data_items_json)
 
-        picked = result['href']
-        link.append(picked)
+    base_anime_url = 'https://myanimelist.net/'
+    titles_and_links = []
 
-    df = pd.DataFrame({'Title': titles})
-    print(df)
+    for item in data_items:
+        title = item['anime_title']
+        relative_url = item['anime_url']
+        full_url = f"{base_anime_url}{relative_url.strip('/')}"
+        titles_and_links.append((title, full_url))
+
+    print('\n================================')
+    print(f"Here is the list of anime you have in {username}'s watch list: ")
+    for i, (title, link) in enumerate(titles_and_links, start=1):
+        print(f"{i}. {title}")
+
+    print('\n================================')
+    print("Let's see how good these anime are")
+    print("================================\n")
+
+    print("Calculating the recommendations...")
 
 
+    
+    listOfAnime=[]
+    #[0][0] tu black lagoon, [0][1] tu linknya
+    for title, link in titles_and_links:
+        print ('Searching for: ', title)
+        response = requests.get(link)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
+        width = float(soup.find('div', class_='recommended__bar')['style'].split(':')[1].split('%')[0])
+        width = round(width, 2)
+        listOfAnime.append([width, title])
 
+print("================================")
+print("Here is the list of anime you have searched: ")
+df = pd.DataFrame(listOfAnime, columns=['Satisfaction', 'Title'])
+df = df.sort_values(by='Satisfaction', ascending=False)
+print(df)
 
+print("================================")
+print("Do you want to save the result to a CSV file so u won't lose it? (y/n)")
+answer = input()
+
+if answer.lower() == 'y':
+    df.to_csv('anime_satisfaction.csv', mode='a', header=not os.path.exists('anime_satisfaction.csv'), index=False)
+    print("The result has been appended to anime_satisfaction.csv")
+else:
+    print("Alright, the result is not saved")
+
+print("Thank you for using MyAnimeList Scraper by Raion! Have a nice day!")
